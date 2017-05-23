ae98704e20730bd453c4535649f4f103b2d96507
Pedro Chinen <chinen93@gmail.com>
2017-05-23 13:27:28 -0300

    Update DATE-UPDATED label on org files

--- files/emacsConfig/init-orgmode.el
+++ files/emacsConfig/init-orgmode.el
@@ -90,9 +90,54 @@
   "Create a list of this week and todo items"
   (interactive)
 
+  ;; Get the Agenda indexed by 'b'
   (org-agenda nil "b")
-  (delete-other-windows)
-)
+
+  ;; Remove other windows so this is the only one visible
+  (delete-other-windows))
 ;; defun my-week-and-todo-list END
+
+
+;; defun pc-study/update-timestamp BEGIN
+(defun my-update-org-timestamp ()
+  "Search for the string 'DATE-UPDATED' and chage the inactive 
+timestamp after it."
+
+  ;; Save excursion so the pointer isn't changed
+  (save-excursion
+
+    ;; Go to the first positon in the buffer
+    (goto-char (point-min))
+
+    ;; Search for the string "DATE-UPDATED: "
+    (if (not (null (search-forward-regexp "DATE-UPDATED: " nil t)))
+
+	;; Text is found: Save the beginning of the text after it
+	(let ((begin (point)))
+	 
+	  ;; Go to the final of the date
+	  (search-forward "]")
+
+	  ;; Delete the date described as [year-month=day DayofWeek]
+	  (delete-region begin (point))
+
+	  ;; Insert date of today
+	  (org-insert-time-stamp (current-time) nil t))
+
+      ;; Text is not found: Message and do nothing
+      (message "DATE-UPDATED does not exist in this buffer"))))
+;; defun pc-study/update-timestamp END
+
+(defun my-orgp ()
+  "Check this file is an org file, is it is execute some functions"
+
+  (if (string-match "\\.org" buffer-file-name)
+      (my-update-org-timestamp)))
+
+;; Add hook before save
+(add-hook 'before-save-hook 'my-orgp)
+
+
+
 
 (provide 'init-orgmode)
