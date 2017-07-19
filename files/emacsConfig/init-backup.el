5a1af794a5791a893075a13d2363caa124664b8d
Pedro Chinen <chinen93@gmail.com>
2017-07-19 20:54:14 -0300

    Backup configurations

--- files/emacsConfig/init-backup.el
+++ files/emacsConfig/init-backup.el
@@ -7,4 +7,8 @@
 (setq auto-save-file-name-transforms
       '((".*" "~/.emacs.d/auto-save-list/" t)))
 
+(setq backup-by-copying t)
+(setq delete-old-versions t)
+(setq version-control t)
+
 (provide 'init-backup)
