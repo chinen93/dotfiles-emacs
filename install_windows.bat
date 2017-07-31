ff70ccefbc052c1b16405d0a1e26375093f49f9b
Pedro Chinen <chinen93@gmail.com>
2017-07-31 11:04:57 -0300

    Remove folder if exist, so the install is always fresh

--- install_windows.bat
+++ install_windows.bat
@@ -5,10 +5,20 @@ SET emacsDirectory=%APPDATA%\.emacs.d\
 SET gitDirectory=%USERPROFILE%\Documents\git\dotfiles-emacs\files\
 ECHO --------------------------------------------------------
 ECHO Moving Emacs Configuration Files Folder into Position
+IF EXIST %emacsDirectory%\init.el (
+   DEL %emacsDirectory%\init.el
+)
 MKLINK /H %emacsDirectory%\init.el %gitDirectory%\emacs
 ECHO --------------------------------------------------------
 ECHO Moving Emacs Initial File into Position
+IF EXIST %APPDATA%\.emacsConfig (
+   RMDIR %APPDATA%\.emacsConfig
+)
 MKLINK /D %APPDATA%\.emacsConfig %gitDirectory%\emacsConfig
+
+IF EXIST %APPDATA%\.snippets (
+   RMDIR %APPDATA%\.snippets
+)
 MKLINK /D %APPDATA%\.snippets %gitDirectory%\snippets
 ECHO --------------------------------------------------------
 
