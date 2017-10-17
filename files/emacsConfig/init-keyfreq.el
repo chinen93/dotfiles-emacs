ca7fb27b3992ea22cb38c92bd3a5d93dc60441fb
Pedro Chinen <chinen93@gmail.com>
2017-10-16 23:18:35 -0200

    Put some commands to ignore on key frequency

--- files/emacsConfig/init-keyfreq.el
+++ files/emacsConfig/init-keyfreq.el
@@ -9,19 +9,31 @@
     (message "Keyfreq - Loaded")
 
     ;; Commands that are not listed in (keyfreq-show)
-    ;; FIXME: add more commands that are not needed to be listed
     (setq keyfreq-excluded-commands
 	  '(self-insert-command
 	    abort-recursive-edit
-	    forward-char
 	    backward-char
+	    backward-delete-char-untabify
+	    c-electric-backspace
+	    company-ignore
+	    delete-backward-char
+	    forward-char
+	    helm-next-line
+	    helm-previous-line
+	    left-char
+	    mouse-drag-region
+	    mouse-set-point
+	    mwheel-scroll
+	    next-line
+	    org-delete-backward-char
+	    org-self-insert-command
 	    previous-line
-	    next-line))
+	    right-char))
 
     ;; Start keyfreq mode
     (keyfreq-mode 1)
 
-    ;; Star key freq auto sabe
+    ;; Star key freq auto sabex
     (keyfreq-autosave-mode 1)))
 
 
