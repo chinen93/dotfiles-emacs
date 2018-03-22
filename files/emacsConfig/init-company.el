1d49d95acef356d99158441b09ff6d12133a17d0
Pedro Chinen <chinen93@gmail.com>
2018-03-22 16:26:19 -0300

    Add comments to company configuration

--- files/emacsConfig/init-company.el
+++ files/emacsConfig/init-company.el
@@ -5,17 +5,22 @@
   :ensure t
   :config
   (progn
-    (setq company-idle-delay 0)
+
+    ;; Set delay to start completition
+    (setq company-idle-delay 0.2)
     (setq company-echo-delay 0)
+
+    ;; Keep the return of company as-is
     (setq company-dabbrev-downcase nil)
 
-    ;; minimimum size to start to search for match
+    ;; Minimimum size to start to search for match
     (setq company-minimum-prefix-length 2)
     (setq company-selection-wrap-around t)
 
-    ;; sort matches by occurrence and backend importance
+    ;; Sort matches by occurrence and backend importance
     (setq company-transformers '(company-sort-by-occurrence
 				 company-sort-by-backend-importance))
+
     (message "Company - Loaded")
 
     ;; Start mode globally
