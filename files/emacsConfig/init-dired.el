de539877ef7c538eabbaf6d06aaa4008d9467526
Pedro Chinen <chinen93@gmail.com>
2018-03-22 16:26:40 -0300

    Add comments to dired configuration

--- files/emacsConfig/init-dired.el
+++ files/emacsConfig/init-dired.el
@@ -2,6 +2,10 @@
 ;; Dired Package
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; -a : show all entries even those "hidden".
+;; -l : use a long listing format.
+;; -H : follow symbolic links.
+;; --group-directories-first : directory before files.
 (setq dired-listing-switches "-alH --group-directories-first")
 
 (provide 'init-dired)
