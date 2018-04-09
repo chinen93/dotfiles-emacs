9666896812184ecce1bc09921a8c9e5abb1144ef
Pedro Chinen <chinen93@gmail.com>
2018-04-09 15:15:55 -0300

    Add configurations for Ledger-cli

---
+++ files/emacsConfig/init-ledger.el
@@ -0,0 +1,13 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Ledger Package
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package ledger-mode
+  :ensure t
+  :config
+  (progn
+    ;; configs here
+    
+    (message "Ledger - Loaded")))
+
+(provide 'init-ledger-mode)
