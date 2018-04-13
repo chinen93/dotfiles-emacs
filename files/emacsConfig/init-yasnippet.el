e25f284efefdac3eb23011912ee9ee1b490461ca
Pedro Chinen <chinen93@gmail.com>
2018-04-13 20:21:26 -0300

    Add snippets and make snippets with fixed identation

--- files/emacsConfig/init-yasnippet.el
+++ files/emacsConfig/init-yasnippet.el
@@ -9,6 +9,9 @@
 ;; Surppress yasnippet backquote changes
 (setq warning-suppress-types '(yasnippet backquote-change))
 
+;; Controls indenting applied to snippets.
+(setq yas-indent-line 'fixed)
+
 ;; Change add Directories when looking for snippets
 (setq yas-snippet-dirs
       ;; Personal Collection
