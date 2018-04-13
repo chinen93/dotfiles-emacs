e79a11819674b87a24e142d96720dc20a51d5b89
Pedro Chinen <chinen93@gmail.com>
2018-04-13 20:21:54 -0300

    Add more configurations to ledger mode

--- files/emacsConfig/init-ledger.el
+++ files/emacsConfig/init-ledger.el
@@ -6,8 +6,23 @@
   :ensure t
   :config
   (progn
-    ;; configs here
+    (message "Ledger - Loaded")
+
+    ;; Clear whole transactions, not individual postings.
+    (setq ledger-clear-whole-transactions t)
+
+    ;; awalker4 mode to clean the ledger buffer
+    ;; https://github.com/awalker4/.dotfiles/blob/master/emacs.d/config.org
+    (defun aw/clean-ledger-on-save ()
+      (interactive)
+      (if (eq major-mode 'ledger-mode)
+          (let ((curr-line (line-number-at-pos)))
+            (ledger-mode-clean-buffer)
+            (line-move (- curr-line 1)))))
+
+    (add-hook 'before-save-hook 'aw/clean-ledger-on-save)
     
-    (message "Ledger - Loaded")))
+    (eval-after-load 'ledger-mode
+      (define-key ledger-mode-map (kbd "M-q") nil))))
 
 (provide 'init-ledger-mode)
