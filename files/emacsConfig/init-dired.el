6c5899f0533f48a52d6be0712ca5e76ed68f4511
Pedro Chinen <chinen93@gmail.com>
2018-06-01 17:22:10 -0300

    add configurations to dired

--- files/emacsConfig/init-dired.el
+++ files/emacsConfig/init-dired.el
@@ -6,6 +6,11 @@
 ;; -l : use a long listing format.
 ;; -H : follow symbolic links.
 ;; --group-directories-first : directory before files.
-(setq dired-listing-switches "-alH --group-directories-first")
+(setq dired-listing-switches "-Alh --group-directories-first --sort=extension")
+
+(defun xah-dired-mode-setup ()
+  "to be run as hook for `dired-mode'."
+  (dired-hide-details-mode 1))
+(add-hook 'dired-mode-hook 'xah-dired-mode-setup)
 
 (provide 'init-dired)
